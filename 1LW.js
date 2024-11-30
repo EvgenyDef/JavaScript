df864e9acb6ea996198bab2d5bfeb62e48700297
EvgenyDef <evgenyzv2005@gmail.com>
2024-11-30 19:41:58 +0400

    2LW Правки

--- 1LW.js
+++ 1LW.js
@@ -1,11 +1,47 @@
-/*Объявите функцию, которая принимает на вход функциональное
-выражение и набор параметров для его вызова, а возвращает результат
-вызова переданного функционального выражения.*/
 
+// 1
+function maxMult(n1, n2, n3) {
+    let array = [n1, n2, n3].sort();
+    return array[1] * array[2];
+}
+
+// 2
+function two(name, title = 'рядовой') {
+    return `${name}, ${title}`;
+}
+
+// 3
 function func(greeting, name, surname) {
     return greeting(name, surname);
 }
 
 const exp = (name, surname) => `Hello, ${name} ${surname}!`;
 
-console.log(func(exp, 'Evgeny', 'Def'));
+// 4
+
+const even = n => n ** 2;
+const odd = n => n ** 3;
+
+function four(element) {
+    return (element % 2 === 0) ? () => element ** 2 : () => element **3; 
+}
+
+// 5
+function five(a) {
+    if (a === 1)
+        return 1;
+    return a * five(a - 1);
+}
+
+
+
+console.log('1: ' + maxMult(5, 8, 1));
+
+console.log('2: ' + two('Олег'));
+console.log('2: ' + two('Олег', 'сержант'));
+
+console.log('3: ' + func(exp, 'Evgeny', 'Def'));
+
+console.log('4: ' + four(5));
+
+console.log('5: ' + five(5));
